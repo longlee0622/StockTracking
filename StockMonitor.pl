9ceb4d51a8430b0f5c3d5cc956b542f661ea0026
Jonas Li <jonasl@nvidia.com>
2015-06-06 15:42:46 +0800

    formatting

--- StockMonitor.pl
+++ StockMonitor.pl
@@ -11,9 +11,9 @@ my $interval;
 
 # process arguments
 Getopt::Long::GetOptions(
-	'input=s'=>\$input,
-	'interval=i'=>\$interval,
-	);
+    'input=s'=>\$input,
+    'interval=i'=>\$interval,
+    );
 
 $interval=3 if (!defined($interval));
 
@@ -24,15 +24,15 @@ close $fh;
 # build curl command
 my $command = "curl -s http://hq.sinajs.cn/list=";
 foreach (@list) {
-	chomp;
-	next if ($_ eq "");
-	my $market;
-	if (/^6/) {
-		$market="sh";
-	}else {
-		$market="sz";
-	}
-	$command = $command.$market.$_.",";
+    chomp;
+    next if ($_ eq "");
+    my $market;
+    if (/^6/) {
+        $market="sh";
+    }else {
+        $market="sz";
+    }
+    $command = $command.$market.$_.",";
 }
 
 
@@ -40,15 +40,15 @@ while (1) {
         print "--------------------------------------\n";
         print "Time      ID      Name\t\tPrice\tChange%\n";
         my @raw=`$command`;
-		foreach (@raw) {
-			chomp;
+        foreach (@raw) {
+            chomp;
             Encode::from_to($_, "gb2312", "utf8");
-			next if ($_ eq "");
-			if(/^var hq_str_[szh]{2}([\d]{6})="([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),.*"/) {
-			my $change = sprintf("%5.2f%%",($5-$4)/$4*100);
+            next if ($_ eq "");
+            if(/^var hq_str_[szh]{2}([\d]{6})="([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),.*"/) {
+            my $change = sprintf("%5.2f%%",($5-$4)/$4*100);
             my $output = $33."  ".$1."  ".$2."\t".$5."\t".$change."\n"; # Time Id Name Price change% 
             print $output;
-		}
-	}
-	sleep($interval);
+        }
+    }
+    sleep($interval);
 }
